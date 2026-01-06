32dd408636469198f349c6959d4ecf229a184922
Rahmatullah <154547965+RahmatullahF@users.noreply.github.com>
2026-01-06 13:32:31 +0430

    Add userdata.sh for server setup and Docker installation

---
+++ userdata.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+hostnamectl set-hostname ${server-name}
+apt update -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+groupadd docker
+usermod -aG docker $USER
+newgrp docker
